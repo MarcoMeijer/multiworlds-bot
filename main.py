f3f2ad05d6112d3a0175f839d91e36e477e1c195
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-01-15 21:36:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import random
+import asyncio
+from discord.ext.commands import Bot 
 
 BOT_PREFIX = (";", "/")
 client = Bot(command_prefix=BOT_PREFIX)
@@ -10,3 +12,14 @@ async def on_ready():
     print(client.user)
     await client.change_presence(game=discord.Game(name=';help'))
 
+async def list_servers():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        print("Current servers in which the bot is running:")
+        for server in client.servers:
+            print(server.name)
+        await asyncio.sleep(600)
+			
+token = os.environ.get("DISCORD_BOT_SECRET")
+client.loop.create_task(list_servers())
+client.run(token)
