7e436ef6d2ba353f03e14f3c683b3584246bac3e
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-01-15 21:34:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import random
+
+BOT_PREFIX = (";", "/")
+client = Bot(command_prefix=BOT_PREFIX)
+client.remove_command("help")
+
+@client.event
+async def on_ready():
+    print("I'm in")
+    print(client.user)
+    await client.change_presence(game=discord.Game(name=';help'))
 
